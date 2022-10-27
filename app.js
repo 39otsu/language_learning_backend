7f4d61cd2b234b6b1a6989bd34ac8a71398f12c4
39otsu <macgyver.codilla39@outlook.jp>
2022-10-26 20:27:49 -0700

    port?

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const search = require('./routes/search');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,7 +24,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 app.use('/dir', dir);
